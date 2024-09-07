ab6044e415de7af98f6a53c30ee2b6f831111bf4
Yoimar Vargas <yoimarnova123@gmail.com>
2024-09-07 00:09:41 -0500

    3 terminado

--- TallerTres/app.js
+++ TallerTres/app.js
@@ -1,13 +1,50 @@
-let api_key ="67Rc8MaxsN5sO1xJUU4Knl1NkGm1oJnb";
-let limit = 20;
-const URL = "api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=${limit}";
+let api_key = "67Rc8MaxsN5sO1xJUU4Knl1NkGm1oJnb";
+let limit = 50;
+const container = document.querySelector(".cards-container");
 
-const getCharacter = async (URL) => {
-    const response = await fetch(URL);
-    const data = await response.json();
+const getGifs = async (url) => {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        container.innerHTML = "";
+
+        data.data.forEach(element => {
+            let gif = element.images.original.url;
+            makeCards(gif);
+        });
     
-    // Itera sobre cada elemento dentro de 'results'
-    data.data[images].forEach(element => {
-                   // Llama a la función makeCards con el array
-    });
+};
+
+function makeCards(gif) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let img = document.createElement("img");
+    img.src = gif;
+    card.appendChild(img);
+
+    container.appendChild(card);
 }
+
+const loadTrendingGifs = () => {
+    const trendingURL = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=${limit}`;
+    getGifs(trendingURL); 
+};
+
+const searchGifs = () => {
+    const searchTerm = document.getElementById("input-character").value;
+
+    if (searchTerm.trim() !== "") {
+        let searchURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchTerm}&limit=${limit}`;
+        getGifs(searchURL);
+    } else {
+        
+        loadTrendingGifs();
+    }
+};
+
+
+loadTrendingGifs();
+
+
+document.getElementById("input-character").addEventListener("input", searchGifs);
